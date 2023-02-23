da45cdee0b14ac1bcafd6a7c7243482731cbdf3e
nguyen hung <you@example.com>
2023-02-23 15:29:39 +0700

    hello

---
+++ cred.py
@@ -0,0 +1,22 @@
+#Twilio Details
+
+account_sid = 'AC16b67ff89856aaf3b7bae8693db61e41'
+auth_token = '089d80ea029b966eb46a3b80f3291bea'
+twilionumber = '+15744656789'
+twiliosmsnumber = '+15744656789'
+
+#FC Bot
+API_TOKEN = "6204857251:AAH6rSpTklODerCsdGWJ29P_sxE_8hkSV3Y"
+
+#Host URL
+callurl = 'https://flask-production-54e7.up.railway.app'
+twiliosmsurl = 'https://flask-production-54e7.up.railway.app/sms'
+
+
+
+
+
+
+
+
+
