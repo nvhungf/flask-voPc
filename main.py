aa37ccde0cdc3d75d905eb544de1b3ccff6cf259
nvhungf <71314788+nvhungf@users.noreply.github.com>
2023-02-23 15:39:15 +0700

    upupupuup

--- main.py
+++ main.py
@@ -1187,6 +1187,6 @@ def handle_recordings(userid):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
 
 
